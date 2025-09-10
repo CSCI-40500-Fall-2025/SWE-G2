00b54a761805f136dde978ced611f32cf0fc0beb
Eric Lin <eric.lin67@myhunter.cuny.edu>
2025-09-09 20:27:23 -0400

    Added class that stores the username and profile pic

---
+++ Capture/src/components/FriendList.tsx
@@ -0,0 +1,9 @@
+export default class FriendList {
+    imageURL: string;
+    username: string;
+    constructor(username: string, imageURL: string) {
+        this.username = username;
+        this.imageURL = imageURL;
+    }
+
+}
